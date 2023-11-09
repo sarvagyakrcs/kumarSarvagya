7f2744a69e6dd9ea257e7c92c6454fa7f3df4cd9
SarvagyaKumar <sarvagyakrcs@gmail.com>
2023-11-09 19:37:51 +0530

    Blog Post Implementation

--- src/data.js
+++ src/data.js
@@ -1,31 +1,50 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const apiUrl = 'https://sarvagyakumarcd22.pythonanywhere.com/posts';
+const apiUrl = 'http://sarvagyakumarcd22.pythonanywhere.com/posts';
+const accessToken = '987677ba11d78e59ebc24dfe48b256016d4ceb9c';
 
-// const fetchLastTenPosts = async () => {
-//     try {
-//         const response = await axios.get(apiUrl);
+let post_data = []; // Define post_data variable here
 
-//         if (response.status === 200) {
-//             const allPosts = response.data; // Assuming the response contains all available posts
-//             const lastTenPosts = allPosts.slice(-10);
-//             const post_data = lastTenPosts.map(post => ({
-//                 id: post.id,
-//                 title: post.title,
-//                 content: post.content,
-//                 date: post.date
-//             }));
+export async function fetchData() {
+    try {
+        const response = await axios.get(apiUrl, {
+            headers: {
+                Authorization: `Token ${accessToken}`
+            }
+        });
 
-//             console.log(post_data); // Logging the last 10 posts in the desired format
-//             return post_data;
-//         } else {
-//             throw new Error('Failed to fetch data');
-//         }
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         return null;
-//     }
-// };
+        if (response.status >= 200 && response.status < 300) {
+            // Successful request (status codes 2xx)
+            const newPosts = response.data.map(post => ({
+                id: post.id,
+                title: post.title,
+                content: post.content,
+                date: post.date
+            }));
+
+            // Add new posts to post_data and maintain only the last 10
+            post_data = post_data.concat(newPosts);
+            if (post_data.length > 10) {
+                post_data = post_data.slice(post_data.length - 10);
+            }
+
+            console.log('Last 10 posts:', post_data);
+
+        } else {
+            console.log('Request was not successful. Status:', response.status);
+            console.error('Error message:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error occurred:', error);
+        if (error.response) {
+            console.error('Error status:', error.response.status);
+            console.error('Error data:', error.response.data);
+        }
+    }
+}
+fetchData();
+
+
 
 const socialMediaLinks = {
     'link-1': {
@@ -53,73 +72,73 @@ const socialMediaLinks = {
 const contactDetails = {
     'Name': 'SARVAGYA KUMAR',
     'Email': 'SARVAGYAKRCS@GMAIL.COM',
-    'Branch' : 'Computer Science and Engineering (Data Science)',
-    'College' : 'R.V. College of Engineering',
-    'City' : 'Bengaluru',
+    'Branch': 'Computer Science and Engineering (Data Science)',
+    'College': 'R.V. College of Engineering',
+    'City': 'Bengaluru',
 }
 
-const post_data = [
-    {
-        "id": 1,
-        "title": "Hello",
-        "content": "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Python is dynamically typed and garbage-collected. It supports multiple programming paradigms, including structured, object-oriented, and functional programming.",
-        "date": "2023-11-09T04:35:31.583009Z"
-    },
-    {
-        "id": 2,
-        "title": "World",
-        "content": "JavaScript is a versatile scripting language used for web development. It enables interactive web pages and is an essential part of web applications. JavaScript allows for asynchronous communication and can be used for both client-side and server-side development.",
-        "date": "2023-11-08T10:20:15.123456Z"
-    },
-    {
-        "id": 3,
-        "title": "Programming",
-        "content": "Java is a popular programming language known for its portability and compatibility across platforms. It is commonly used for developing mobile apps, web applications, and enterprise software. Java uses an object-oriented approach and is statically typed.",
-        "date": "2023-11-07T15:45:00.987654Z"
-    },
-    {
-        "id": 4,
-        "title": "Data Science",
-        "content": "R programming language is widely used for data analysis and statistical computing. It offers a variety of statistical techniques and graphical methods. R provides a rich ecosystem for data science and is used by analysts and statisticians.",
-        "date": "2023-11-06T20:55:42.345678Z"
-    },
-    {
-        "id": 5,
-        "title": "Artificial Intelligence",
-        "content": "C++ is a powerful programming language used extensively in the development of system software, games, and applications that require high performance. It provides low-level memory manipulation and is an extension of the C programming language.",
-        "date": "2023-11-05T08:10:30.876543Z"
-    },
-    {
-        "id": 6,
-        "title": "Machine Learning",
-        "content": "Ruby is a dynamic and object-oriented programming language known for its simplicity and productivity. It is used in web development and has a strong community contributing to its frameworks and libraries.",
-        "date": "2023-11-04T12:25:55.234567Z"
-    },
-    {
-        "id": 7,
-        "title": "Web Development",
-        "content": "PHP is a widely-used server-side scripting language suited for web development. It is embedded into HTML and works well with databases. PHP allows developers to create dynamic and interactive web pages.",
-        "date": "2023-11-03T06:30:12.765432Z"
-    },
-    {
-        "id": 8,
-        "title": "Frontend Technologies",
-        "content": "Swift is a powerful and intuitive programming language primarily used for iOS, macOS, watchOS, and tvOS app development. It is known for its safety, speed, and modern features.",
-        "date": "2023-11-02T17:40:05.678901Z"
-    },
-    {
-        "id": 9,
-        "title": "Backend Development",
-        "content": "Go, also known as Golang, is a statically typed, compiled language. It is recognized for its efficiency and concurrency support, making it suitable for backend development and system programming.",
-        "date": "2023-11-01T09:15:20.543210Z"
-    },
-    {
-        "id": 10,
-        "title": "Mobile Development",
-        "content": "Kotlin is a statically typed programming language used for developing modern Android applications. It is concise, safe, interoperable with Java, and widely adopted by Android developers.",
-        "date": "2023-10-31T14:50:00.112233Z"
-    }
-]
+// const post_data = [
+//     {
+//         "id": 1,
+//         "title": "Hello",
+//         "content": "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation. Python is dynamically typed and garbage-collected. It supports multiple programming paradigms, including structured, object-oriented, and functional programming.",
+//         "date": "2023-11-09T04:35:31.583009Z"
+//     },
+//     {
+//         "id": 2,
+//         "title": "World",
+//         "content": "JavaScript is a versatile scripting language used for web development. It enables interactive web pages and is an essential part of web applications. JavaScript allows for asynchronous communication and can be used for both client-side and server-side development.",
+//         "date": "2023-11-08T10:20:15.123456Z"
+//     },
+//     {
+//         "id": 3,
+//         "title": "Programming",
+//         "content": "Java is a popular programming language known for its portability and compatibility across platforms. It is commonly used for developing mobile apps, web applications, and enterprise software. Java uses an object-oriented approach and is statically typed.",
+//         "date": "2023-11-07T15:45:00.987654Z"
+//     },
+//     {
+//         "id": 4,
+//         "title": "Data Science",
+//         "content": "R programming language is widely used for data analysis and statistical computing. It offers a variety of statistical techniques and graphical methods. R provides a rich ecosystem for data science and is used by analysts and statisticians.",
+//         "date": "2023-11-06T20:55:42.345678Z"
+//     },
+//     {
+//         "id": 5,
+//         "title": "Artificial Intelligence",
+//         "content": "C++ is a powerful programming language used extensively in the development of system software, games, and applications that require high performance. It provides low-level memory manipulation and is an extension of the C programming language.",
+//         "date": "2023-11-05T08:10:30.876543Z"
+//     },
+//     {
+//         "id": 6,
+//         "title": "Machine Learning",
+//         "content": "Ruby is a dynamic and object-oriented programming language known for its simplicity and productivity. It is used in web development and has a strong community contributing to its frameworks and libraries.",
+//         "date": "2023-11-04T12:25:55.234567Z"
+//     },
+//     {
+//         "id": 7,
+//         "title": "Web Development",
+//         "content": "PHP is a widely-used server-side scripting language suited for web development. It is embedded into HTML and works well with databases. PHP allows developers to create dynamic and interactive web pages.",
+//         "date": "2023-11-03T06:30:12.765432Z"
+//     },
+//     {
+//         "id": 8,
+//         "title": "Frontend Technologies",
+//         "content": "Swift is a powerful and intuitive programming language primarily used for iOS, macOS, watchOS, and tvOS app development. It is known for its safety, speed, and modern features.",
+//         "date": "2023-11-02T17:40:05.678901Z"
+//     },
+//     {
+//         "id": 9,
+//         "title": "Backend Development",
+//         "content": "Go, also known as Golang, is a statically typed, compiled language. It is recognized for its efficiency and concurrency support, making it suitable for backend development and system programming.",
+//         "date": "2023-11-01T09:15:20.543210Z"
+//     },
+//     {
+//         "id": 10,
+//         "title": "Mobile Development",
+//         "content": "Kotlin is a statically typed programming language used for developing modern Android applications. It is concise, safe, interoperable with Java, and widely adopted by Android developers.",
+//         "date": "2023-10-31T14:50:00.112233Z"
+//     }
+// ]
 
 
 
@@ -129,4 +148,4 @@ const jumbotronAnimatedText = {
     'thirdSentence': 'I am Vengence⚔️'
 }
 
-export  {socialMediaLinks, contactDetails, post_data, jumbotronAnimatedText};
+export { socialMediaLinks, contactDetails, post_data, jumbotronAnimatedText };
