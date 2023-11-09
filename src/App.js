87f4c638d8a7ae624563f3f773f51bdc68a71458
SarvagyaKumar <sarvagyakrcs@gmail.com>
2023-11-09 22:21:54 +0530

    personal blog debugging

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import { React, useState } from 'react';
-// components
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/navBar/navbar';
 import Footer from './components/footer/footer';
 import Jumbotron from './components/jumbotron/jumbotron';
-// data
-import { socialMediaLinks, contactDetails, jumbotronAnimatedText, post_data, newPosts } from './data'
-// css
+import { socialMediaLinks, contactDetails, jumbotronAnimatedText } from './data';
 import './input.css';
-// packages
 import PersonalBlog from './components/personalBlog/personalBlog';
 
 function App() {
-
     const [mode, setMode] = useState('darkMode');
+    const [posts, setPosts] = useState([]); // State to store the fetched posts
+
+    const apiUrl = 'https://sarvagyakumarcd22.pythonanywhere.com/posts';
+    const accessToken = '987677ba11d78e59ebc24dfe48b256016d4ceb9c';
+
+    // Function to fetch the posts
+    const fetchPosts = async () => {
+        try {
+            const response = await fetch(apiUrl, {
+                headers: {
+                    Authorization: `Token ${accessToken}`
+                }
+            });
+    
+            if (response.ok) {
+                const data = await response.json();
+    
+                if (Array.isArray(data)) {
+                    const slicedPosts = data.slice(0, 10); // Retrieve the first 10 posts
+                    setPosts(slicedPosts); // Update the state with fetched posts
+                } else {
+                    console.error('Response data is not an array:', data);
+                }
+            } else {
+                console.error('Failed to fetch data:', response.status, response.statusText);
+            }
+        } catch (error) {
+            console.error('Error fetching posts:', error);
+        }
+    };
+    
+
+    useEffect(() => {
+        fetchPosts(); // Fetch posts when the component mounts
+    }, []); // Empty dependency array means it will run once when the component mounts
 
     return (
         <>
@@ -29,7 +59,7 @@ function App() {
 
             {/* personal-blog */}
             <PersonalBlog
-                posts={post_data}
+                posts={posts} // Pass fetched posts as a prop
                 contactDetails={contactDetails}
                 mode={mode}
                 setMode={setMode}
--- src/data.js
+++ src/data.js
@@ -1,77 +1,38 @@
 import axios from 'axios';
 
-const apiUrl = 'https://sarvagyakumarcd22.pythonanywhere.com/posts';
-const accessToken = '987677ba11d78e59ebc24dfe48b256016d4ceb9c';
 
-let post_data = [
-    {
-        "id": 10000000,
-        "title": "The Rise of Artificial Intelligence",
-        "content": "Artificial intelligence continues to transform industries, revolutionizing the way we live and work. Its applications in healthcare, finance, and automation are reshaping the future.",
-        "date": "2023-11-09T04:35:31.583009Z"
-    },
-    {
-        "id": 20000000,
-        "title": "Blockchain: Beyond Cryptocurrency",
-        "content": "While initially known for cryptocurrencies, blockchain's potential reaches far beyond. Its decentralized nature has implications in supply chain management, voting systems, and more.",
-        "date": "2023-11-09T04:38:31.003298Z"
-    },
-    {
-        "id": 30000000,
-        "title": "Quantum Computing: The Next Frontier",
-        "content": "Quantum computing promises unprecedented computational power, potentially solving complex problems that classical computers struggle with, revolutionizing encryption and scientific simulations.",
-        "date": "2023-11-09T14:01:19.612363Z"
-    },
-    {
-        "id": 40000000,
-        "title": "Cybersecurity in an Interconnected World",
-        "content": "With the growing interconnectivity, cybersecurity becomes pivotal. Advancements in AI-driven security systems, zero-trust architectures, and biometric authentication are reshaping defense strategies.",
-        "date": "2023-11-09T14:05:29.123456Z"
-    },
-    {
-        "id": 50000000,
-        "title": "The Era of Internet of Things (IoT)",
-        "content": "IoT is transforming our daily lives with smart homes, connected devices, and industrial automation. Its impact on healthcare, agriculture, and urban planning is continually expanding.",
-        "date": "2023-11-09T14:10:42.987654Z"
-    }
-]
-
-let newPosts = [];
-
-export async function fetchData() {
-    try {
-        const response = await axios.get(apiUrl, {
-            headers: {
-                Authorization: `Token ${accessToken}`
-            }
-        });
-
-        // Successful request (status codes 2xx)
-        newPosts = response.data
-        // console.log('New posts:', newPosts);
-        // console.log({'Type of post_Data' : typeof(post_data)});
-        // console.log({'post_Data' : post_data});
-        // console.log({'Type of newPosts' : typeof(newPosts)});
-        // console.log({'newPosts' : newPosts});
-
-        // Add new posts to post_data and maintain only the last 10
-        post_data = post_data.concat(newPosts);
-        if (post_data.length > 10) {
-            post_data = post_data.slice(post_data.length - 10);
-        }
-        console.log('Last 10 posts:', post_data);
-
-        console.log({'post_Data final' : post_data});
-    } catch (error) {
-        console.error('Error occurred:', error);
-        if (error.response) {
-            console.error('Error status:', error.response.status);
-            console.error('Error data:', error.response.data);
-        }
-    }
-}
-
-fetchData();
+// let post_data = [
+//     {
+//         "id": 10000000,
+//         "title": "The Rise of Artificial Intelligence",
+//         "content": "Artificial intelligence continues to transform industries, revolutionizing the way we live and work. Its applications in healthcare, finance, and automation are reshaping the future.",
+//         "date": "2023-11-09T04:35:31.583009Z"
+//     },
+//     {
+//         "id": 20000000,
+//         "title": "Blockchain: Beyond Cryptocurrency",
+//         "content": "While initially known for cryptocurrencies, blockchain's potential reaches far beyond. Its decentralized nature has implications in supply chain management, voting systems, and more.",
+//         "date": "2023-11-09T04:38:31.003298Z"
+//     },
+//     {
+//         "id": 30000000,
+//         "title": "Quantum Computing: The Next Frontier",
+//         "content": "Quantum computing promises unprecedented computational power, potentially solving complex problems that classical computers struggle with, revolutionizing encryption and scientific simulations.",
+//         "date": "2023-11-09T14:01:19.612363Z"
+//     },
+//     {
+//         "id": 40000000,
+//         "title": "Cybersecurity in an Interconnected World",
+//         "content": "With the growing interconnectivity, cybersecurity becomes pivotal. Advancements in AI-driven security systems, zero-trust architectures, and biometric authentication are reshaping defense strategies.",
+//         "date": "2023-11-09T14:05:29.123456Z"
+//     },
+//     {
+//         "id": 50000000,
+//         "title": "The Era of Internet of Things (IoT)",
+//         "content": "IoT is transforming our daily lives with smart homes, connected devices, and industrial automation. Its impact on healthcare, agriculture, and urban planning is continually expanding.",
+//         "date": "2023-11-09T14:10:42.987654Z"
+//     }
+// ]
 
 
 
@@ -112,4 +73,5 @@ const jumbotronAnimatedText = {
     'thirdSentence': 'I am Vengence⚔️'
 }
 
-export { socialMediaLinks, contactDetails, post_data, jumbotronAnimatedText, newPosts };
+export { socialMediaLinks, contactDetails, jumbotronAnimatedText };
+
