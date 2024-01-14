19d4c69276824fd919073a25f1bb6007e32980e6
SarvagyaKumar <sarvagyakrcs@gmail.com>
2024-01-14 21:36:06 +0530

    miscellaneous

--- src/components/Login/loginForm.jsx
+++ src/components/Login/loginForm.jsx
@@ -24,7 +24,6 @@ const LoginForm = (props) => {
             const result = await signInWithPopup(auth, googleProvider);
             setUser(result.user);
             localStorage.setItem('userDetails', JSON.stringify(user));
-            console.log(localStorage.getItem('userDetails'));
             localStorage.setItem('isLoggedIn', true);
         } catch (error) {
             console.error('Error signing in with Google:', error);
