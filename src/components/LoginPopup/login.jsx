100253dfce01071570a75bee06d3e79aec5790cf
SarvagyaKumar <sarvagyakrcs@gmail.com>
2024-01-14 21:15:32 +0530

    made signin Popup responsive

--- src/components/LoginPopup/login.jsx
+++ src/components/LoginPopup/login.jsx
@@ -1,42 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './LoginPopup.css';
-import { useNavigate } from 'react-router-dom';
 import cross from '../../assets/cross.png'
-import spaceman from '../../assets/spaceman.jpeg'
+import loon from '../../assets/loon.png'
+import minion from '../../assets/minion.png'
+import barbarian from '../../assets/barbarian.png'
+import golem from '../../assets/golem.png'
 import LoginForm from '../Login/loginForm';
 
 const LoginPopup = (props) => {
-    const navigate = useNavigate();
     const user = JSON.parse(localStorage.getItem('userDetails'));
 
-    const notLogin = (
-        <div className="rightBoxLoginPopup">
-            <h1>Please Sign In With Google</h1>
-            <LoginForm setLoginPopup={props.setLoginPopup} setIsLoggedin={props.setIsLoggedin}/>   
-        </div>
-    );
+    const images = [loon, barbarian, minion, golem];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    const loggedIn = (
-        <div className="rightBoxLoginPopup">
-            <h1>Please Sign In With Google</h1>
-            <LoginForm setLoginPopup={props.setLoginPopup} setIsLoggedin={props.setIsLoggedin}/>   
-        </div>
-    );
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            // Switch to the next image after 2 seconds
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(intervalId);
+    }, [currentImageIndex, images.length]);
+    
 
     return (
         <div className="mainLoginPopup">
             <div className="loginPopup-box">
                 <div className="leftBoxLoginPopup">
-                    <img src={spaceman} alt="" />
+                    <img src={images[currentImageIndex]} alt="currentImage" onClick={() => props.setLoginPopup(false)} />
                 </div>
                 <div className="rightBoxLoginPopup">
-                    <h1>Please Sign In With Google</h1>
+                    <h1 className="rightBoxLoginPopup-h1">Please Sign In With Google</h1>
                     <div className="mainLogin">
                         <LoginForm setIsLoggedin={props.setIsLoggedin}/>  
                     </div> 
                 </div>
                 <div className="crossButtonLoginPopup">
-                    <button className='crossButton' onClick={() => props.setLoginPopup(false)}><img src={cross} /></button>
+                    <button className='crossButton' onClick={() => props.setLoginPopup(false)}><img src={cross} alt='Hello' /></button>
                 </div>
             </div>
         </div>
